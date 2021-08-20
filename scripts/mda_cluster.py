ceab10730bf9474af4d61f9a3e803b8170255b7c
jkelowitt <58435724+jkelowitt@users.noreply.github.com>
2021-08-20 13:05:05 -0700

    Formatting

--- scripts/mda_cluster.py
+++ scripts/mda_cluster.py
@@ -3,12 +3,13 @@
 # Clemson University
 # 2019 Jun 10
 
-import sys
-import os
 import argparse
-import numpy as np
+import os
+import sys
+
 import MDAnalysis as mda
 import networkx as nx
+import numpy as np
 
 # For PointNet modules
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -18,10 +19,11 @@ sys.path.append(os.path.join(BASE_DIR, '../'))
 import mda_custom.nsgrid.nsgrid_rsd as nsgrid
 from models.pointnet import PointNet
 
-def main():
 
-    #Parse Arguments
-    parser = argparse.ArgumentParser(description='Uses MDAnalysis and PointNet to identify largest cluster of solid-like atoms')
+def main():
+    # Parse Arguments
+    parser = argparse.ArgumentParser(
+        description='Uses MDAnalysis and PointNet to identify largest cluster of solid-like atoms')
     parser.add_argument('--weights', help='folder containing network weights to use', type=str, required=True)
     parser.add_argument('--nclass', help='number of classes', type=int, required=True)
     parser.add_argument('--trjpath', help='path to gro/xtc files', type=str, required=True)
@@ -32,30 +34,30 @@ def main():
     args = parser.parse_args()
 
     # Import topology
-    u = mda.Universe(args.trjpath+'.gro',
-                     args.trjpath+'.xtc')
-    
+    u = mda.Universe(args.trjpath + '.gro',
+                     args.trjpath + '.xtc')
+
     # File to write output
-    f_summary = open(args.outname+'_summary.mda','w')
-    f_class = open(args.outname+'_class.mda','w')
-    
+    f_summary = open(args.outname + '_summary.mda', 'w')
+    f_class = open(args.outname + '_class.mda', 'w')
+
     f_summary.write("# Time, Largest_cls_solid, n_liq, n_fcc, n_hcp, n_bcc, n_solid\n")
 
     # Here is where we initialize the pointnet
-    pointnet = PointNet(n_points=args.maxneigh,n_classes=args.nclass,weights_dir=args.weights)
+    pointnet = PointNet(n_points=args.maxneigh, n_classes=args.nclass, weights_dir=args.weights)
 
     # Loop over trajectory
     for ts in u.trajectory:
         # Generate neighbor list
-        print("Generating neighbor list") 
-        nlist = nsgrid.FastNS(args.cutoff*10.0,u.atoms.positions,
-                                      ts.dimensions).self_search()
+        print("Generating neighbor list")
+        nlist = nsgrid.FastNS(args.cutoff * 10.0, u.atoms.positions,
+                              ts.dimensions).self_search()
 
         # Extract required info 
         ndxs = nlist.get_indices()
         dxs = nlist.get_dx()
         dists = nlist.get_distances()
-        print("Extracted all relevant information") 
+        print("Extracted all relevant information")
 
         samples = []
         # Prepare samples to send through pointnet
@@ -63,8 +65,8 @@ def main():
             ## Sort neighbors by distance (so that we can 
             ## normalize all distances such that the distance
             ## to the closest neighbor is 1.0 )
-            nneigh = int(len(dxs[i])/3)
-            np_dxs = np.asarray(dxs[i]).reshape([nneigh,3])
+            nneigh = int(len(dxs[i]) / 3)
+            np_dxs = np.asarray(dxs[i]).reshape([nneigh, 3])
             sort_order = np.asarray(dists[i]).argsort()
             # Sort neighbors by distance 
             np_dxs = np_dxs[sort_order]
@@ -72,7 +74,7 @@ def main():
                 np_dxs /= np.linalg.norm(np_dxs[0])
             # Now correctly size/pad the point cloud
             if nneigh < args.maxneigh:
-                np_dxs = np.pad(np_dxs,[(0, args.maxneigh-nneigh), (0, 0)],'constant',)
+                np_dxs = np.pad(np_dxs, [(0, args.maxneigh - nneigh), (0, 0)], 'constant', )
             elif nneigh > args.maxneigh:
                 np_dxs = np_dxs[:args.maxneigh]
 
@@ -81,14 +83,14 @@ def main():
 
         # And convert to np array
         np_samples = np.asarray(samples)
-        print("Frame {}, Shape sent to pointnet: {}".format(ts.frame,np_samples.shape))
+        print("Frame {}, Shape sent to pointnet: {}".format(ts.frame, np_samples.shape))
         sys.stdout.flush()
-    
+
         # Send sample through inference 
         results = pointnet.infer_nolabel(np_samples)
         results = np.asarray(results)
-    
-        print("Frame {}, Results returned from pointnet, shape {}".format(ts.frame,results.shape))
+
+        print("Frame {}, Results returned from pointnet, shape {}".format(ts.frame, results.shape))
         sys.stdout.flush()
 
         # Extract different atom types
@@ -104,8 +106,8 @@ def main():
 
         # We need neighbor lists for connectivity cutoff 
         # Using 15.0 Angstroms (mda units) here
-        nlist = nsgrid.FastNS(15.0,u.atoms.positions,
-                                    ts.dimensions).self_search()
+        nlist = nsgrid.FastNS(15.0, u.atoms.positions,
+                              ts.dimensions).self_search()
 
         pairs = nlist.get_pairs()
 
@@ -114,17 +116,19 @@ def main():
         G.add_edges_from(pairs)
         G.remove_nodes_from(liquid_atoms)
         largest_cluster = max(nx.connected_component_subgraphs(G), key=len)
-       
-        f_summary.write("{:8.3f}{:8d}{:8d}{:8d}{:8d}{:8d}{:8d}\n".format(ts.time,len(largest_cluster),liquid_atoms.shape[0],
-            fcc_atoms.shape[0],hcp_atoms.shape[0],bcc_atoms.shape[0],solid_atoms.shape[0]))
+
+        f_summary.write(
+            "{:8.3f}{:8d}{:8d}{:8d}{:8d}{:8d}{:8d}\n".format(ts.time, len(largest_cluster), liquid_atoms.shape[0],
+                                                             fcc_atoms.shape[0], hcp_atoms.shape[0], bcc_atoms.shape[0],
+                                                             solid_atoms.shape[0]))
 
         for node in largest_cluster:
-            f_class.write("{:10d}{:8d}{:8d}\n".format(ts.frame+1,node,results[node]))
+            f_class.write("{:10d}{:8d}{:8d}\n".format(ts.frame + 1, node, results[node]))
 
     f_summary.close()
     f_class.close()
 
+
 # Boilerplate command to exec main
 if __name__ == "__main__":
     main()
-
