2fb33ffc4d49c7a03ed3e48b01c6ef17abff92e8
jkelowitt <58435724+jkelowitt@users.noreply.github.com>
2021-08-20 13:07:08 -0700

    Formatting

--- scripts/prepare_train.py
+++ scripts/prepare_train.py
@@ -3,12 +3,13 @@
 # Clemson University
 # 2019 Jun 11
 
-import sys
-import os
-import math
-import numpy as np
 import argparse
+import math
+import os
+import sys
+
 import MDAnalysis as mda
+import numpy as np
 
 # For Cython nlist
 # PROVIDE AN ABSOLUTE PATH TO MAKE THIS IMPORT WORK
@@ -17,12 +18,13 @@ import MDAnalysis as mda
 sys.path.append('$HOME/GenStrIde/mda_custom')
 import nsgrid.nsgrid_rsd as nsgrid
 
+
 def main():
     # Argument parsing
     args = get_args()
 
     # List of all classes
-    classes = ['liq','fcc','hcp','bcc']
+    classes = ['liq', 'fcc', 'hcp', 'bcc']
     nclass = len(classes)
 
     # Get list of all subdirectories
@@ -30,14 +32,14 @@ def main():
     # Narrow directory list to specified classes
     # Assumes (T,P) conditions in directories named 'phase_pPPPtTTT'
     dirs = [d for d in dirs if d.split("_")[0] in classes]
-  
+
     # Get all files
     files_gro = ["%s/prod.gro" % d for d in dirs]
     files_xtc = ["%s/prod.xtc" % d for d in dirs]
 
     assert len(files_gro) == len(files_xtc), \
-            "Error, unequal number of .gro and .xtc files found" 
- 
+        "Error, unequal number of .gro and .xtc files found"
+
     # Initialize lists for samples and labels
     samples = []
     labels = []
@@ -46,7 +48,6 @@ def main():
     count = 0
     mean = 0
     m2 = 0
-
 
     # Read in data for each file
     for fcount in range(len(files_gro)):
@@ -63,16 +64,16 @@ def main():
         # Loop over trajectory
         for ts in u.trajectory:
             # Select atoms at random for samples
-            sel = np.random.choice(u.atoms.n_atoms,size=args.n_select,replace=False)
-            #sel = [13518]
-            #print(sel)
+            sel = np.random.choice(u.atoms.n_atoms, size=args.n_select, replace=False)
+            # sel = [13518]
+            # print(sel)
             # Create neighbor list for atoms
-            nlist = nsgrid.FastNS(args.cutoff*10.0,u.atoms.positions,ts.dimensions).search(u.atoms[sel].positions)
+            nlist = nsgrid.FastNS(args.cutoff * 10.0, u.atoms.positions, ts.dimensions).search(u.atoms[sel].positions)
             ndxs = nlist.get_indices()
             dxs = nlist.get_dx()
             dists = nlist.get_distances()
             for i in range(len(sel)):
-                np_dxs = np.asarray(dxs[i]).reshape(-1,3)
+                np_dxs = np.asarray(dxs[i]).reshape(-1, 3)
                 sort = np.argsort(dists[i])
                 # Remove the (0,0,0) atom
                 vals = np_dxs[sort][1:]
@@ -85,15 +86,15 @@ def main():
                 # Calc avg/stdev of nneigh with Welford Alg.
                 count += 1
                 delta = nneigh - mean
-                mean += delta/count
+                mean += delta / count
                 delta2 = nneigh - mean
-                m2 += delta*delta2
+                m2 += delta * delta2
                 # Zero padding
                 if nneigh > args.max_neigh:
-                    sample = np.resize(vals,(args.max_neigh,3))
+                    sample = np.resize(vals, (args.max_neigh, 3))
                 elif nneigh < args.max_neigh:
-                    npad = args.max_neigh  - nneigh
-                    sample = np.vstack((vals,np.zeros((npad,3))))
+                    npad = args.max_neigh - nneigh
+                    sample = np.vstack((vals, np.zeros((npad, 3))))
                 else:
                     sample = vals
                 # Append sample and label
@@ -106,7 +107,7 @@ def main():
 
     ## Here is some extra pre-processing of training data ##
     # Extract idxs for different classes
-    idxs = [np.where(labels[:,i] == 1)[0] for i in range(nclass)]
+    idxs = [np.where(labels[:, i] == 1)[0] for i in range(nclass)]
     # Extract samples/labels for each class
     samples_list = [samples[idx] for idx in idxs]
     labels_list = [labels[idx] for idx in idxs]
@@ -114,10 +115,10 @@ def main():
     [np.random.shuffle(samples_list[i]) for i in range(nclass)]
     # 2. Confirm equal number of samples per phase
     for i in range(nclass):
-        print("Total number of available samples for class %s: %d" % (classes[i],samples_list[i].shape[0]))
+        print("Total number of available samples for class %s: %d" % (classes[i], samples_list[i].shape[0]))
         assert samples_list[i].shape[0] >= args.n_samples, \
             "Error, only %d samples in class %s but requested %d." \
-            % (samples_list[i].shape[0],classes[i],args.n_samples)
+            % (samples_list[i].shape[0], classes[i], args.n_samples)
     samples_list = [samples_list[i][:args.n_samples] for i in range(nclass)]
     labels_list = [labels_list[i][:args.n_samples] for i in range(nclass)]
     # 3. Restack
@@ -125,7 +126,7 @@ def main():
     labels = np.vstack(labels_list)
     # 4. Normalize each sample such that distance to closest atom is 1.0 units
     for k in range(samples.shape[0]):
-        samples[k,...] = samples[k,...]/np.linalg.norm(samples[k][0])
+        samples[k, ...] = samples[k, ...] / np.linalg.norm(samples[k][0])
 
     # save output files
     np.save(args.out_name + '_scaled_shuffled_equal_samples.npy', samples)
@@ -133,24 +134,26 @@ def main():
 
     # Finalize and print avg/stdev nneigh
     print("Mean nneigh: %f" % mean)
-    print("Stdev nneigh: %f" % ((math.sqrt(m2/count))))
+    print("Stdev nneigh: %f" % ((math.sqrt(m2 / count))))
+
 
 def get_args():
-
-    #Parse Arguments
+    # Parse Arguments
     parser = argparse.ArgumentParser(description='Create datasets for pointnet training for crystal structure ID')
     parser.add_argument('--path', help='path to simulations for generating training data', type=str, required=True)
     parser.add_argument('--out_name', help='file prefix to save training dataset', type=str, required=True)
     parser.add_argument('--cutoff', help='neighbor cutoff for point clouds', type=float, required=True)
-    parser.add_argument('--max_neigh',help='max neighbors in point clouds',type=int,required=True)
-    parser.add_argument('--n_select', help='number training samples to extract from each frame of a simulation', type=int, required=False,default=5)
-    parser.add_argument('--n_samples', help='total number training samples per phase', type=int, required=False,default=100000)
+    parser.add_argument('--max_neigh', help='max neighbors in point clouds', type=int, required=True)
+    parser.add_argument('--n_select', help='number training samples to extract from each frame of a simulation',
+                        type=int, required=False, default=5)
+    parser.add_argument('--n_samples', help='total number training samples per phase', type=int, required=False,
+                        default=100000)
 
     args = parser.parse_args()
 
     return args
 
+
 # Boilerplate notation to run main fxn
 if __name__ == "__main__":
     main()
-
